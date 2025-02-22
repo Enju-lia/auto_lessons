1e02ba12a23ee1d39c114cb07ff8a4c918cea858
Olya <miralisu@gmail.com>
2025-02-22 17:04:52 +0300

    Добавили библиотеку testng, написали первый тест

--- Autotest/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Autotest/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,18 +7,9 @@ public class MyFirstProgram {
 		Square s = new Square(5);
 		Rectangle re = new Rectangle(2, 3);
 		System.out.println("Hello, world!");
-		System.out.println("Площадь квадрата со сторонами " + s.r +  " = " + area(s));
+		System.out.println("Площадь квадрата со сторонами " + s.r +  " = " + s.area());//обьект который вызывается в методе area
 		System.out.println("У нашего квадрата длина стороны = " + s.r);
-		System.out.println( "Площадь прямоугольника со сторонами " + re.a + " и " + re.b + " = " + area(re));
-		System.out.println("Сумма площадей квадрата со сторонами " + s.r + " и прямоугольника со сторонами " + re.a + " и " + re.b + " равна " + sumOfAreas(s, re));
+		System.out.println( "Площадь прямоугольника со сторонами " + re.a + " и " + re.b + " = " + re.area());
 	}
-	public static double area (Square s) {
-		return s.r * s.r;
-}
-	public static double area (Rectangle s) {
-		return s.a * s.b;
-	}
-	public static double sumOfAreas (Square a, Rectangle b){
-		return area(a) + area(b);
-	}
+
 }
--- Autotest/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ Autotest/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -2,7 +2,12 @@ package ru.stqa.pft.sandbox;
 
 public class Square {
     public double r;
-    public Square (double r) {
+
+    public Square(double r) {
         this.r = r;
+    }
+
+    public double area() {
+        return this.r * this.r;
     }
 }
